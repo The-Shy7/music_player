2cbb02fba183ea82e44e911b3e6d056e5c34e716
tahmit <tahmit@uw.edu>
2020-09-14 12:52:52 -0700

    Updated playlist with more songs

--- src/Playlist.js
+++ src/Playlist.js
@@ -10,5 +10,23 @@ export default [
         cover: '/assets/images/RAM.jpg',
         title: 'Instant Crush',
         artist: ['Daft Punk']
+    },
+    {
+        src: '/assets/audio/Alones.mp3',
+        cover: '/assets/images/Alones.jpg',
+        title: 'Alones',
+        artist: ['Aqua Timez']
+    },
+    {
+        src: '/assets/audio/Luv(Sic)Part3.mp3',
+        cover: '/assets/images/Luv(Sic)Part3.jpg',
+        title: 'Luv(sic.), Pt. 3 (feat. Shing02)',
+        artist: ['Nujabes']
+    },
+    {
+        src: '/assets/audio/ShunkanSent.mp3',
+        cover: '/assets/images/ShunkanSent.jpg',
+        title: 'Shunkan Sentimental',
+        artist: ['SCANDAL']
     }
 ];
